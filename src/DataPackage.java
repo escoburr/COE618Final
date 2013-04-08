0cc56ff99c96adfa393e1b61c87287c3b09eb297
ytwytw <zhangjunhuaz@gmail.com>
2013-04-07 23:48:39 -0400

    Fixed Server Hang Issue

--- src/DataPackage.java
+++
@@ -1,9 +0,0 @@
-
-import java.io.Serializable;
-
-public class DataPackage implements Serializable {
-
-    public float x = 0.0f;
-    public float y = 0.0f;
-    public String username = "";
-}
---
+++ src/Package1/DataPackage.java
@@ -0,0 +1,10 @@
+package Package1;
+
+import java.io.Serializable;
+
+public class DataPackage implements Serializable {
+
+    public float x = 0.0f;
+    public float y = 0.0f;
+    public String username = "";
+}
